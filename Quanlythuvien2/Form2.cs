c2e02431c8604b31c263eff332f51c866c8de059
QuachNguyet <anhnguyet24292863@gmail.com>
2021-10-02 23:03:02 +0700

    remove comment

--- Quanlythuvien2/Form2.cs
+++ Quanlythuvien2/Form2.cs
@@ -13,13 +13,9 @@ namespace Quanlythuvien2
 {
     public partial class Form2 : Form
     {
-        // làm gì có cái cú pháp nào khai báo biến mà k có propertype???
-     // Does not exist in curent context => tức là không tồn tại class này trong cùng 1 context (project).
-     // xem n có gợi ý sửa k
-        private IObjectContainer db = Db4oFactory.OpenFile("CSDL Sach"); // (0) có phải cái này k => thế thì em phải khai báo db ơ đâu cho đúng nào?
-                                                                         // "trong phạm vi của hàm laydl() hoặc lớp cha của nó" => hỉu hông
-        int i = 0;
+        private IObjectContainer db = Db4oFactory.OpenFile("CSDL Sach");
         private IObjectSet result;
+        private int i;
         public Form2()
         {
             InitializeComponent();
@@ -32,20 +28,10 @@ namespace Quanlythuvien2
         private void laydl()
         {
             Sach book = new Sach();
-            // làm gì có cái cú pháp nào khai báo biến mà k có propertype???
-            // ví dụ em khai báo 1 biến 
-            // int x= 1
-            // chứ làm gì có cái cú pháp 
-            // x = 1. mà x chưa được định nghĩa
-            // hỉu hông ...
-            // còn 1 lỗi thứ 2.
-            // cái db có tồn tại trong phạm vi của hàm laydl() hoặc lớp cha của nó k??? => ... chưa tao
-            // rùi nếu tạo thì nó là cái gì nào? => /...là dữ liệu lấy ra từ db40 ý (0)
             result = db.QueryByExample(book);
             if(result.Count>0)
             {
                 Sach o = (Sach)result.Next();
-                // lại là lỗi k tồn tại textBox1
                 Ma.Text = o.MaSach;
                 Tensach.Text = o.TenSach;
                 NhaXB.Text = o.NhaXB;
@@ -55,17 +41,7 @@ namespace Quanlythuvien2
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            // không có contructor nào truyền vào đống param này cả
-            // sau khi tạo constructor rồi thì có thể khởi tạo 1 object Sach bằng constructorr như này
             Sach b1 = new Sach(Ma.Text, Tensach.Text, NhaXB.Text, Dongia.Text);
-            // a dạy 1 cách khác mà k cần constructor
-            //Sach b2 = new Sach()
-            //{
-            //    MaSach = Ma.Text,
-            //    TenSach = Tensach.Text,
-            //    NhaXB = NhaXB.Text,
-            //    DonGia = Dongia.Text
-            //};
             db.Store(b1);
             MessageBox.Show("Thêm thông tin thành công");
             laydl();
